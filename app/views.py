12ebcb1fe22371295331098468b1a4bcb37bfded
wyqbupt <wyqcsnet@yeah.net>
2016-06-05 09:24:35 -0700

    updaate User model

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, loginmanager, oid
 from .forms import LoginForm
+from .models import User, ROLE_USER, ROLE_ADMIN
+
+@loginmanager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 @app.route('/index')
@@ -22,11 +28,14 @@ def index():
         posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="'+form.openid.data+'",remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template("login.html",
 	title = 'Sign In', 
 	form = form,
