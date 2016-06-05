8dce9fc5b9a640928ec9a381c8a5a775855af218
wyqbupt <wyqcsnet@yeah.net>
2016-06-05 09:06:45 -0700

    add Login config

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
+from os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+loginmanager = LoginManager()
+loginmanager.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views,models
