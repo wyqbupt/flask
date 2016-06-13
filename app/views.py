5ef29c1bfe8b6d9af5f9d9402aa1a7fcd9772fbb
wyqbupt <wyqcsnet@yeah.net>
2016-06-13 07:02:02 -0700

    add login view function

--- app/views.py
+++ app/views.py
@@ -8,6 +8,20 @@ from .models import User, ROLE_USER, ROLE_ADMIN
 def load_user(id):
     return User.query.get(int(id))
 
+@app.route('/login', methods=['GET','POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
+
 @app.route('/')
 @app.route('/index')
 def index():
